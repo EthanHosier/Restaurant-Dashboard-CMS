a52ed8d4de0747b0329d77157eb1dccdeb80e973
EthanHosier <ethanjhosier@gmail.com>
2023-07-05 22:07:04 +0100

    removed unneccessary stuff

--- src/components/table/bookings-columns.tsx
+++ src/components/table/bookings-columns.tsx
@@ -1,44 +1,17 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 import { isToday, isTomorrow, truncateComment } from "@/lib/utils"
-import { deleteDoc, doc } from "firebase/firestore";
-import { db } from "@/firebase/config";
+
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+
 
 import EditCutomerNotes from "./EditCustomerNotes";
 import Actionsbcs from "./Actions-bcs";
@@ -147,65 +120,6 @@ export const columns: ColumnDef<Booking>[] = [
     id: "actions",
     cell: ({ row }) => {
       //console.log(row.original.comments)
-
-      const date: Date = row.getValue("date");
-
-      const dateToCheck = new Date(date);
-
-
-      const currentDate = new Date();
-      currentDate.setHours(0, 0, 0, 0);
-      dateToCheck.setHours(0, 0, 0, 0);
-
-      let reason = "";
-
-      const onReasonChange = (e: any) => {
-        reason = e;
-      }
-
-      const handleCancelBooking = async () => {
-
-        //console.log(row.original.id)
-
-        //console.log(row.original.email);
-
-        try {
-          const ref = doc(db, "bookings", row.original.id);
-          await deleteDoc(ref);
-
-          const apiKey = 'willybum';
-
-          const URL = 'https://us-central1-management-restaurants.cloudfunctions.net/api/bookings/handleCancellation';
-
-          //(row.original)
-
-          const {
-            email,
-            id,
-            restaurantTitle,
-            location,
-            messageId
-          } = row.original;
-
-          const body = { email, id, restaurantTitle, location, reason, messageId };
-
-          /*const res =*/ await fetch(URL, {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json',
-              'x-api-key': apiKey,
-            },
-            body: JSON.stringify(body)
-          })
-
-
-        } catch (error) {
-          console.log("error cancelling booking ", error)
-        }
-      }
-
-
-
 
       return (
         <Actionsbcs row={row}/>
